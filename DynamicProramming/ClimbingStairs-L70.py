03a034a888f4b007a49dfeb089089c389e7b5f34
Amit Bhalerao <amit.bhalerao1616@gmail.com>
2022-06-11 15:47:13 -0700

    https://leetcode.com/problems/climbing-stairs/

---
+++ DynamicProramming/ClimbingStairs-L70.py
@@ -0,0 +1,23 @@
+'''
+TC: O(2^n)
+SC: O(n)
+'''
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def totalWaysToClimb(currentStair:int, target: int,memo:dict)->int:
+            if currentStair == target:
+                return 1
+            if currentStair > target:
+                return 0
+            if currentStair in memo:
+                return memo[currentStair]
+            onestep = totalWaysToClimb(currentStair+1,n,memo)
+            twostep = totalWaysToClimb(currentStair+2,n,memo)
+            
+            memo[currentStair] = onestep+twostep
+            
+            return memo[currentStair]
+        
+        memo = {}
+        return totalWaysToClimb(0,n,memo)
